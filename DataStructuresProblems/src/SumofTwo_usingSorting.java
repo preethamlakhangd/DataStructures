f783ff9dee69244e413bc70db642a04e3175cdc4
preethamlakhangd <preetham.lakhan@yahoo.com>
2022-01-28 01:45:34 -0500

    SumofTwo 3st Solution
    Time complexity O(nlogN) and Space O(1)

---
+++ DataStructuresProblems/src/SumofTwo_usingSorting.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class SumofTwo_usingSorting {
+
+    public  int[] twoNumberSum(int[] ar, int targetSum) {
+        //
+       Arrays.sort(ar);
+       int left =0;
+       int right = ar.length -1 ;
+       while(left<right){
+           if(ar[left]+ar[right]== targetSum){
+               return new int[]{ar[left],ar[right]};
+           }else if (ar[left]+ar[right] < targetSum){
+               left++;
+           }else{
+               right--;
+           }
+       }
+
+        return new int[0];
+    }
+
+    public static void main(String[] args) {
+        SumofTwo_usingSorting sumofTwo = new SumofTwo_usingSorting();
+        int [] ar = new int[]{3, 5, -4, 8, 11, 1, -1, 6};
+        int target = 10;
+
+        System.out.print(Arrays.toString(sumofTwo.twoNumberSum(ar,target) ));
+    }
+}
